9f2b071c6d541e8958861ed293d86dc6dd4dae27
unknown <melissa@txable.com>
2018-03-03 12:18:34 -0500

    fix node module

---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'nXDf3UmXseewQcVp5dUjwLfp2',
+  consumer_secret: 'XG5LY2b1kaRulwbqS3tgCx8qBwaAExSOJTgtlLmFbeuVrfmiir',
+  access_token_key: '957079751084199937-RlKFzFQUUS3VCQ15BSjfnz8NJlgHl45',
+  access_token_secret: 'UWyVwgoFiM9YD1qH1t4qnN7HpDWr6ahWrli7c9VbCdkDN',
+};
+
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
